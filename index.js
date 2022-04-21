081cfb27cb0239c174f253b529cab90351bb47f4
frsiam <siam.frs@gmail.com>
2022-04-21 12:31:25 +0600

    hardcoded data insertion complete

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ app.use(express.json());
 
 const uri = "mongodb+srv://dbuser1:O4bB4oaSv4snS7Fm@cluster0.mdofc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db('foodExpress').collection('user');
+        const user = {name: 'islam', email: "islam@muslim.com"};
+        const result = await userCollection.insertOne(user);
+        console.log(`user inserted with id: ${result.insertedId}`)
+    }
+    finally{
+        // await client.close();
+    }
+}
 
-client.connect(err => {
-  const collection = client.db("foodExpress").collection("users");
-  console.log('db connected')
-  // perform actions on the collection object
-  client.close();
-});
+run().catch(console.dir)
 
 
 app.get('/', (req,res)=>{
