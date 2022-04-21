2bf675b5ba4c8d03b009a569bea8d3e9e31f0382
frsiam <siam.frs@gmail.com>
2022-04-21 11:08:13 +0600

    add use midleware of cors and express.json

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// use midleware 
+app.use(cors())
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('running my node crud server');
 })
 
 app.listen(port, ()=>{
-    console.log('server is running')
+    console.log('server is running');
 })
